f2118dc8dc20aa1d4002cd7b6016c7aaa1279179
Zarzuket <davide.bajo@gmail.com>
2021-09-16 16:43:13 +0200

    adding img

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const app = new Vue(
+    {
+        el: "#root",
+        data: {
+            titolo: "qwerty",
+            immagine: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmultiplayer.it%2Fgiochi%2Fdota-2-per-pc.html&psig=AOvVaw1XjlaewIguwOi_mnIB6LTz&ust=1631889441667000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiqxofcg_MCFQAAAAAdAAAAABAD",
+        }
+    }
+)
